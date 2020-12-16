505233fca532ec4fe68f453d151beb31095a3f1e
Matthews <212689101@ge.com>
2020-12-16 18:51:35 -0500

    Working version need README

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(column, value, cb) {
+        orm.insertOne("burgers", column, value, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function (burgerId, cb) {
+        orm.updateOne("burgers", "devoured", burgerId, function (res) {
+          cb(res);
+        });
+      }
+};
+
+module.exports = burger;
